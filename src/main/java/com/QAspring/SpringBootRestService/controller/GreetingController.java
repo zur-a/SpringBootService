dddcc5eddd949faaac73f6e72a97b9fba3e880fb
Zura <jur.and.jr42@gmail.com>
2023-04-20 08:02:39 -0300

    Implementation of GreetingController

--- src/main/java/com/QAspring/SpringBootRestService/controller/GreetingController.java
+++ src/main/java/com/QAspring/SpringBootRestService/controller/GreetingController.java
@@ -1,5 +1,23 @@
 package com.QAspring.SpringBootRestService.controller;
 
-public class GreetingController {
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+@Autowired
+Greeting greeting;
+
+AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name")String name) {
+		greeting.setId(counter.incrementAndGet());
+		greeting.setContent("This is a simple project made by " + name);
+		return greeting;
+	}
 }
