76180af17c32b54efc9db47f8fe5469a04f64b5f
Zura <jur.and.jr42@gmail.com>
2023-05-01 17:40:39 -0300

    Created LibraryRepositoryCustom interface

--- src/main/java/com/QAspring/SpringBootRestService/controller/LibraryController.java
+++ src/main/java/com/QAspring/SpringBootRestService/controller/LibraryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -70,4 +71,10 @@ public class LibraryController {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 		}
 	}
+	
+	@GetMapping("/getBook/author")
+	public Library getBookByAuthor(@RequestParam(value="name") String author) {
+		return null;
+	}
+	
 }
---
+++ src/main/java/com/QAspring/SpringBootRestService/repository/LibraryRepositoryCustom.java
@@ -0,0 +1,8 @@
+package com.QAspring.SpringBootRestService.repository;
+
+import java.util.List;
+import com.QAspring.SpringBootRestService.controller.Library;
+
+public interface LibraryRepositoryCustom {
+	List<Library> findAllByAuthor(String author);
+}
