649b7f2150fa2f6e3a74aed3487dac625ac6d3e1
Zura <jur.and.jr42@gmail.com>
2023-05-15 17:24:07 -0300

    Added tests for checking book existance in SpringBootRestServiceApplicationTests

--- src/test/java/com/QAspring/SpringBootRestService/SpringBootRestServiceApplicationTests.java
+++ src/test/java/com/QAspring/SpringBootRestService/SpringBootRestServiceApplicationTests.java
@@ -1,6 +1,8 @@
 package com.QAspring.SpringBootRestService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,16 @@ class SpringBootRestServiceApplicationTests {
 	@Test
 	public void checkBuildId() {
 		assertEquals("978-85-4400-000-714", bookservice.idBuilder("978-85-4400-000-7", 14));
+	}
+	
+	@Test
+	public void checkBookExists() {
+		assertTrue(bookservice.checkBookExists("978-85-4400-000-714"));
+	}
+	
+	@Test
+	public void checkBookDoesNotExists() {
+		assertFalse(bookservice.checkBookExists("000-00-0000-000-001"));
 	}
 	
 	
