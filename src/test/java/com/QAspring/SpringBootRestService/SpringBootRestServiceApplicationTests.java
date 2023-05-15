8a4cd63e8724525d439493de65d4a3970f1d6969
Zura <jur.and.jr42@gmail.com>
2023-05-15 17:09:17 -0300

    Added checkBuildId test to SpringBootRestServiceApplicationTests

--- src/test/java/com/QAspring/SpringBootRestService/SpringBootRestServiceApplicationTests.java
+++ src/test/java/com/QAspring/SpringBootRestService/SpringBootRestServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package com.QAspring.SpringBootRestService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.QAspring.SpringBootRestService.service.BookService;
 
 @SpringBootTest
 class SpringBootRestServiceApplicationTests {
-
+	
+	@Autowired
+	BookService bookservice;
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void checkBuildId() {
+		assertEquals("978-85-4400-000-714", bookservice.idBuilder("978-85-4400-000-7", 14));
 	}
 
 }
