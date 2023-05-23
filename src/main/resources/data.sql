79d2d2e3e36df41e5bbafc7ae19556df094e85a5
Zura <jur.and.jr42@gmail.com>
2023-05-23 16:25:11 -0300

    Created SpringBootServiceIT class for integration test using TestRestTemplate

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Storage(id, title, isbn, aisle, author) VALUES ('978-65-8823-952-005', 'Arte poética', '978-65-8823-952-0', '05', 'Horácio');
+INSERT INTO Storage(id, title, isbn, aisle, author) VALUES ('978-65-5525-078-705', 'Odes', '978-65-5525-078-7', '05', 'Horácio');
+INSERT INTO Storage(id, title, isbn, aisle, author) VALUES ('978-85-7326-550-715', 'Eneida', '978-85-7326-550-7', '15', 'Virgílio');
---
+++ src/test/java/com/QAspring/SpringBootRestService/SpringBootRestServiceIT.java
@@ -0,0 +1,10 @@
+package com.QAspring.SpringBootRestService;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootRestServiceIT {
+	//Integration test cases
+	//Uses TestRestTemplate
+		//Rest Assured is a viable option
+}
