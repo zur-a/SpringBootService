63b94d534d213fb72d8c328fdc22bacfb265b828
Zura <jur.and.jr42@gmail.com>
2023-05-24 16:56:18 -0300

    Added addBookTest to SpringBootRestServiceIT

--- src/test/java/com/QAspring/SpringBootRestService/SpringBootRestServiceIT.java
+++ src/test/java/com/QAspring/SpringBootRestService/SpringBootRestServiceIT.java
@@ -1,10 +1,18 @@
 package com.QAspring.SpringBootRestService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
+import com.QAspring.SpringBootRestService.controller.Book;
 
 @SpringBootTest
 public class SpringBootRestServiceIT {
@@ -12,18 +20,44 @@ public class SpringBootRestServiceIT {
 	//Uses TestRestTemplate
 		//Rest Assured is a viable option
 	//Active profile in application.properties should be h2 for the tests to run
+	
+	public Book buildBook() {
+		Book book = new Book();
+		book.setTitle("O divã ocidento-oriental");
+		book.setAuthor("Goethe");
+		book.setIsbn("978-85-7448-307-8");
+		book.setAisle(42);
+		book.setId("978-85-7448-307-842");
+		return book;
+	}
+	
+	@Test
+	public void addBookTest() throws Exception {
+		TestRestTemplate rest = new TestRestTemplate();
+		
+		//Creating a header object to send to HttpEntity
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(MediaType.APPLICATION_JSON);
+		
+		//Creating the request object
+		HttpEntity<Book> request = new HttpEntity<Book>(buildBook(), header);
+		
+		//Creating the response as a string given the url of the service, the request object and
+		ResponseEntity<String> response = rest.postForEntity("http://localhost:8080/addBook", request, String.class);
+		
+		//Asserting the status code and the id returned in the unique field
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		assertEquals(buildBook().getId(), response.getHeaders().get("unique").get(0));
+
+	}
+	
 	@Test
 	public void getBookByAuthorTest() throws Exception {
 		TestRestTemplate rest = new TestRestTemplate();
 		
 		//Creating the response as a string given the url of the service
 		ResponseEntity<String> response = rest.getForEntity("http://localhost:8080/getBook/author?authorName=Horácio", String.class);
-		
-		//Printing the status code
-		System.out.println(response.getStatusCode());
-		System.out.println(response.getBody());
-		
-		//
+	
 		String expectedResult = 
 				"[\r\n"
 				+ "    {\r\n"
@@ -42,7 +76,7 @@ public class SpringBootRestServiceIT {
 				+ "    }\r\n"
 				+ "]";
 		
-		//Asserting
+		//Asserting the expected result with the response retuned
 		JSONAssert.assertEquals(expectedResult, response.getBody(), false);
 	}
 	
